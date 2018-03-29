804e67cb80cd861e012d5a8abc7e89a6f815ab6c
arun.tanwar3 <arun.tanwar3@gmail.com>
2018-03-29 15:35:53 +0530

    Add GET/todos route

--- server/server.js
+++ server/server.js
@@ -26,11 +26,20 @@ app.post('/todos',(req,res)=> {
    })
 });
 
-
+ app.get('/todos',(req,res)=> {
+   Todo.find().then((todos)=>{
+     res.send({
+     	todos
+     });
+   },(e)=> {
+       res.status(400).send(e);
+ });
+ });
 app.listen(3000,()=> {
 	console.log('Server is up on port 3000');
 });
 
+module.exports={app};
 /*var Todo =mongoose.model('Todo',{
 	text: {
          type:String,
