a4031303d099eca9020ccfc253e63b5e8b28bc88
arun.tanwar3 <arun.tanwar3@gmail.com>
2018-03-30 00:44:41 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');*/
 var express=require('express');
 var bodyparser=require('body-parser');
 
-
+const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -34,6 +34,30 @@ app.post('/todos',(req,res)=> {
    },(e)=> {
        res.status(400).send(e);
  });
+ });
+
+ app.get('/todos/:id',(req,res)=>{ 
+    var id=req.params.id;
+
+    if(!ObjectID.isValid(id))
+    {
+    	return res.status(400).send();
+    }
+
+    Todo.findById(id).then((todo)=> {
+      if(!todo)
+      {
+      	return res.status(404).send();
+      }
+
+      res.send({todo});
+
+    }).catch((err)=> {
+    	res.status(400).send();
+    
+    });
+
+
  });
 app.listen(3000,()=> {
 	console.log('Server is up on port 3000');
